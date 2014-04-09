c68c5ace568a81c10dab4dfd3ae8d166e328b405
Jack Guan <gjj@cs.dartmouth.edu>
2014-04-09 16:40:20 -0400

    Add the robust stuff to the basic homework

--- hw2/hw2_client/src/client.c
+++ hw2/hw2_client/src/client.c
@@ -12,220 +12,213 @@
 
 static int welcomed = 0;
 
-int create_socket()
-{
-    int sock;
+int create_socket() {
+	int sock;
 
-    sock = socket(AF_INET , SOCK_STREAM , 0);
-    if (sock == -1)
-    {
-        printf("Could not create socket");
-    }
-    // puts("Socket created");
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock == -1) {
+		printf("Could not create socket");
+	}
+	// puts("Socket created");
 
-    return sock;
+	return sock;
 }
 
-struct sockaddr_in config_server(int port)
-{
-    struct sockaddr_in server;
+struct sockaddr_in config_server(int port) {
+	struct sockaddr_in server;
 
-    server.sin_addr.s_addr = inet_addr("129.170.213.101");
-    // server.sin_addr.s_addr = inet_addr("127.0.0.1");
-    server.sin_family = AF_INET;
-    server.sin_port = htons( port );
-    //server.sin_port = htons( 8888 );
+	server.sin_addr.s_addr = inet_addr("129.170.213.101");
+	// server.sin_addr.s_addr = inet_addr("127.0.0.1");
+	server.sin_family = AF_INET;
+	server.sin_port = htons(port);
+	//server.sin_port = htons( 8888 );
 
-    return server;
+	return server;
 }
 
-char* msg_for_operation(int selection)
-{
-    //void *block = malloc(2000);
-    //memset(block, 0, s);
-    char* message;
+char* msg_for_operation(int selection) {
+	//void *block = malloc(2000);
+	//memset(block, 0, s);
+	char* message;
 
-    printf("Which sensor would you like to read : \n \
+	printf(
+			"Which sensor would you like to read : \n \
 \n \
 \t(1) Water temperature\n \
 \t(2) Reactor temperature\n \
 \t(3) Power level\n\n\
 Selection: ");
-    scanf("%d", &selection);
+	scanf("%d", &selection);
 
-    switch(selection){
-    case 1:
-        message = "WATER TEMPERATURE";
-        break;
-    case 2:
-        message = "REACTOR TEMPERATURE";
-        break;
-    case 3:
-        message = "POWER LEVEL";
-        break;
-    default:
-        break;
-    }
+	switch (selection) {
+	case 1:
+		message = "WATER TEMPERATURE";
+		break;
+	case 2:
+		message = "REACTOR TEMPERATURE";
+		break;
+	case 3:
+		message = "POWER LEVEL";
+		break;
+	default:
+		message = "\0";
+		break;
+	}
 
-    return message;
+	return message;
 }
 
-char* get_time_str(char server_reply[])
-{
-    time_t raw_time , current_time2;
-    char* c_time_string, c_time_string2;
-    char time_str[200];
+char* get_time_str(char server_reply[]) {
+	time_t raw_time, current_time2;
+	char* c_time_string, c_time_string2;
+	char time_str[200];
 
-    /* Obtain current time as seconds elapsed since the Epoch. */
-    memcpy( time_str, &server_reply[0], 10);
-    time_str[10] = '\0';
-    //time_str = "1346426869";
-    raw_time  = atol(time_str);
+	/* Obtain current time as seconds elapsed since the Epoch. */
+	memcpy(time_str, &server_reply[0], 10);
+	time_str[10] = '\0';
+	//time_str = "1346426869";
+	raw_time = atol(time_str);
 
-    if (raw_time  == ((time_t)-1))
-    {
-        (void) fprintf(stderr, "Failure to compute the current time.");
-        return 0;
-    }
+	if (raw_time == ((time_t) -1)) {
+		(void) fprintf(stderr, "Failure to compute the current time.");
+		return 0;
+	}
 
-    /* Convert to local time format. */
-    c_time_string = ctime(&raw_time );
-    //puts(c_time_string);
+	/* Convert to local time format. */
+	c_time_string = ctime(&raw_time);
+	//puts(c_time_string);
 
-    if (c_time_string == NULL)
-    {
-        (void) fprintf(stderr, "Failure to convert the current time.");
-        return 0;
-    }
+	if (c_time_string == NULL) {
+		(void) fprintf(stderr, "Failure to convert the current time.");
+		return 0;
+	}
 
-    /* Print to stdout. */
-    c_time_string[strlen(c_time_string) - 1] = '\0';
-    return c_time_string;
+	/* Print to stdout. */
+	c_time_string[strlen(c_time_string) - 1] = '\0';
+	return c_time_string;
 }
 
-char* get_value_string(char server_reply[])
-{
-    char value_string[100];
-    memcpy( value_string, &server_reply[11], strlen(server_reply) - 11);
-    value_string[strlen(server_reply) - 12] = '\0';
-    return value_string;
+char* get_value_string(char server_reply[]) {
+	char value_string[100];
+	memcpy(value_string, &server_reply[11], strlen(server_reply) - 11);
+	value_string[strlen(server_reply) - 12] = '\0';
+	return value_string;
 }
 
-char* comm_with_server(int sock, struct sockaddr_in server, int phase)
-{
-    char server_reply[2000], return_msg[2000], msg[2000];
-    char* message;
-    static int selection;
-    memset(server_reply, 0, strlen(server_reply));
-    memset(return_msg, 0, strlen(return_msg));
-    memset(msg, 0, strlen(msg));
+char* comm_with_server(int sock, struct sockaddr_in server, int phase) {
+	char server_reply[2000], return_msg[2000], msg[2000];
+	char* message;
+	static int selection;
+	memset(server_reply, 0, strlen(server_reply));
+	memset(return_msg, 0, strlen(return_msg));
+	memset(msg, 0, strlen(msg));
 
-    while(1)
-    {
-        if( phase == GET_AUTH ){
-            message = "AUTH secretpassword";
-        }else if ( phase == GET_CONN ){
-            message = "AUTH networks";
-        }else if ( phase == OPERATION ){
-            message = msg_for_operation(selection);
-        }else if ( phase == CLOSE ){
-            message = "CLOSE";
-        }
-        //printf("Enter message : ");
-        //scanf(" %[^\n]s", message);
+	while (1) {
+		if (phase == GET_AUTH) {
+			message = "AUTH secretpassword";
+		} else if (phase == GET_CONN) {
+			message = "AUTH networks";
+		} else if (phase == OPERATION) {
+			message = msg_for_operation(selection);
+		} else if (phase == CLOSE) {
+			message = "CLOSE";
+		}
+		//printf("Enter message : ");
+		//scanf(" %[^\n]s", message);
 
-        //Send some data
-        strcat(msg, message);
-        strcat(msg, "\n");
-        if( send(sock , msg , strlen(msg) , 0) < 0)
-        {
-            puts("Send failed");
-            return 1;
-        }
+		if (message[0] != 0) {
+			//Send some data
+			strcat(msg, message);
+			strcat(msg, "\n");
+			if (send(sock, msg, strlen(msg), 0) < 0) {
+				puts("Send failed");
+				return 1;
+			}
 
-        //Receive a reply from the server
-        //puts(server_reply);
-        if( recv(sock , server_reply , 2000 , 0) < 0)
-        {
-            puts("recv failed");
-            break;
-        }
+			//Receive a reply from the server
+			//puts(server_reply);
+			if (recv(sock, server_reply, 2000, 0) < 0) {
+				puts("recv failed");
+				break;
+			}
+		}
 
-        //puts("Server reply :");
-        //puts(server_reply);
-        if( phase == GET_AUTH ){
-            memcpy( return_msg, &server_reply[41], 5 );
-            return_msg[5] = '\0';
-        }else if ( phase == OPERATION) {
-        	strcat(return_msg, "\n\t");
-            strcat(return_msg, "The last ");
-            strcat(return_msg, message);
-            strcat(return_msg, " was taken ");
-            strcat(return_msg, get_time_str(server_reply));
-            strcat(return_msg, " and was ");
-            strcat(return_msg, get_value_string(server_reply));
-            strcat(return_msg, "\n\n");
-        }else{
-            memcpy( return_msg, &server_reply[11], strlen(server_reply) );
-            return_msg[strlen(server_reply) ] = '\0';
-        }
-        return return_msg;
-    }
+		//puts("Server reply :");
+		//puts(server_reply);
+		if (phase == GET_AUTH) {
+			memcpy(return_msg, &server_reply[41], 5);
+			return_msg[5] = '\0';
+		} else if (phase == OPERATION) {
+			if (message[0] == 0) {
+				strcat(return_msg, "\n");
+				strcat(return_msg, "*** Invalid selection.");
+				strcat(return_msg, "\n");
+			} else {
+				strcat(return_msg, "\n\t");
+				strcat(return_msg, "The last ");
+				strcat(return_msg, message);
+				strcat(return_msg, " was taken ");
+				strcat(return_msg, get_time_str(server_reply));
+				strcat(return_msg, " and was ");
+				strcat(return_msg, get_value_string(server_reply));
+				strcat(return_msg, "\n\n");
+			}
+		} else {
+			memcpy(return_msg, &server_reply[11], strlen(server_reply));
+			return_msg[strlen(server_reply)] = '\0';
+		}
+		return return_msg;
+	}
 }
 
-int talk_to_3mile(int port)
-{
-    int sock;
-    char* port_str;
-    struct sockaddr_in server;
+int talk_to_3mile(int port) {
+	int sock;
+	char* port_str;
+	struct sockaddr_in server;
 
-    // Create socket
-    sock = create_socket();
+	// Create socket
+	sock = create_socket();
 
-    // Configure server info
-    if(port == -1)
-        server = config_server(47789);
-    else
-        server = config_server(port);
+	// Configure server info
+	if (port == -1)
+		server = config_server(47789);
+	else
+		server = config_server(port);
 
-    // Connect to remote server
-    if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
-    {
-        perror("connect failed. Error");
-        return 1;
-    }
+	// Connect to remote server
+	if (connect(sock, (struct sockaddr *) &server, sizeof(server)) < 0) {
+		perror("connect failed. Error");
+		return 1;
+	}
 
-    if( port == -1 ){
-        // Get auth port
-        port_str = comm_with_server(sock, server, GET_AUTH);
-        if(!welcomed)
-        {
-            puts("WELCOME TO THE THREE MILE ISLAND SENSOR NETWORK\n\n");
-            welcomed = 1;
-        }
-    } else {
-        // hand shake
-        port_str = comm_with_server(sock, server, GET_CONN);
-        // operation
-        port_str = comm_with_server(sock, server, OPERATION);
-        puts(port_str);
-        // say bye
-        port_str = comm_with_server(sock, server, CLOSE);
-    }
-    close(sock);
-    return atoi(port_str);
+	if (port == -1) {
+		// Get auth port
+		port_str = comm_with_server(sock, server, GET_AUTH);
+		if (!welcomed) {
+			puts("WELCOME TO THE THREE MILE ISLAND SENSOR NETWORK\n\n");
+			welcomed = 1;
+		}
+	} else {
+		// hand shake
+		port_str = comm_with_server(sock, server, GET_CONN);
+		// operation
+		port_str = comm_with_server(sock, server, OPERATION);
+		puts(port_str);
+		// say bye
+		port_str = comm_with_server(sock, server, CLOSE);
+	}
+	close(sock);
+	return atoi(port_str);
 }
 
-int main()
-{
-    int port;
+int main() {
+	int port;
 
-    while(1){
-        port = talk_to_3mile(-1);
-        talk_to_3mile(port);
-    }
+	while (1) {
+		port = talk_to_3mile(-1);
+		talk_to_3mile(port);
+	}
 
-    return 0;
+	return 0;
 }
-
 
--- hw2/hw2_server/src/server.c
+++ hw2/hw2_server/src/server.c
@@ -1,232 +1,200 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <string.h>
+/*
+ * File:   main.c
+ * Author: starlight36
+ *
+ * Created on 2012年9月19日, 下午8:45
+ */
 
-#define GET_AUTH 0
-#define GET_CONN 1
-#define OPERATION 2
-#define CLOSE 3
+#include <stdio.h>        // for printf
+#include <stdlib.h>        // for exit
+#include <string.h>        // for bzero
+#include <fcntl.h>
+#include <unistd.h>
+#include <netinet/in.h>    // for sockaddr_in
+#include <sys/types.h>    // for socket
+#include <sys/socket.h>    // for socket
+#include <sys/stat.h>
+#include <pthread.h>
 
-static int welcomed = 0;
+#define SERVER_PORT 47789
+#define LISTEN_QUEUE_LENGTH 20
+#define BUFFER_SIZE 1024
+#define MAX_THREAD_NUM 5
 
-int create_socket()
+#define min(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a < _b ? _a : _b; })
+
+int thread_count = 0;
+char auth_passwd[] = "AUTH secretpassword\n";
+char auth_network[] = "AUTH networks\n";
+pthread_t threads[MAX_THREAD_NUM];
+
+/**
+ * the thread function to handle multiple connections
+ * @param conn_socket
+ */
+int handle_clients(int conn_socket);
+int server_socket_setup(int port);
+
+/*
+ * main
+ */
+int main(int argc, char** argv)
 {
-    int sock;
+	int server_socket = server_socket_setup(SERVER_PORT);
 
-    sock = socket(AF_INET , SOCK_STREAM , 0);
-    if (sock == -1)
+	// handling new coming request
+	bzero(&threads, sizeof(pthread_t) * MAX_THREAD_NUM);
+	while (1)
+	{
+		struct sockaddr_in client_addr;
+		socklen_t length = sizeof(client_addr);
+
+		// connect with client
+		int client_conn = accept(server_socket, (struct sockaddr*) &client_addr, &length);
+		if (client_conn < 0)
+		{
+			printf("Server Accept Failed!\n");
+			return EXIT_FAILURE;
+		}else{
+			puts("client found!");
+		}
+
+		// creating new thread
+		int pthread_err = pthread_create(threads + (thread_count++), NULL, (void *)handle_clients, (void *)client_conn);
+		if (pthread_err != 0)
+		{
+			printf("Create thread Failed!\n");
+			return EXIT_FAILURE;
+		}
+
+	}
+	close(server_socket);
+	return (EXIT_SUCCESS);
+}
+
+int server_socket_setup(int port)
+{
+	// build server Socket
+	int server_socket = socket(PF_INET, SOCK_STREAM, 0);
+	if (server_socket < 0) {
+		printf("Socket create failed.\n");
+		return EXIT_FAILURE;
+	}
+	else
+	{
+		int opt = 1;
+		setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof (opt));
+	}
+
+	// set server configuration
+	struct sockaddr_in server_addr;
+	bzero(&server_addr, sizeof (server_addr));
+
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = htons(INADDR_ANY);
+	server_addr.sin_port = htons(port);
+
+	// bind the port
+	if (bind(server_socket, (struct sockaddr*) &server_addr, sizeof (server_addr)))
+	{
+		printf("Bind port %d failed.\n", port);
+		return -1;
+	}
+
+	// listening
+	if (listen(server_socket, LISTEN_QUEUE_LENGTH))
+	{
+		 printf("Server Listen Failed!");
+		 return EXIT_FAILURE;
+	}
+
+	return server_socket;
+}
+
+int str_compare(char buff1[], char buff2[]){
+	return memcmp(buff1, buff2, min(sizeof(buff1), sizeof(buff2)));
+}
+
+int tell_port_num(int conn_socket)
+{
+	char buffer[BUFFER_SIZE], msg[BUFFER_SIZE], num_str[88];
+	int port_num = 49153, server_socket = -1;
+	bzero(buffer, BUFFER_SIZE);
+	bzero(msg, BUFFER_SIZE);
+
+    if( recv(conn_socket , buffer , BUFFER_SIZE , 0) < 0)
     {
-        printf("Could not create socket");
-    }
-    // puts("Socket created");
-
-    return sock;
-}
-
-struct sockaddr_in config_server(int port)
-{
-    struct sockaddr_in server;
-
-    server.sin_addr.s_addr = inet_addr("129.170.213.101");
-    //server.sin_addr.s_addr = inet_addr("127.0.0.1");
-    server.sin_family = AF_INET;
-    server.sin_port = htons( port );
-    //server.sin_port = htons( 8888 );
-
-    return server;
-}
-
-char* msg_for_operation(int selection)
-{
-    //void *block = malloc(2000);
-    //memset(block, 0, s);
-    char* message;
-
-    printf("Which sensor would you like to read : \n \
-\n \
-\t(1) Water temperature\n \
-\t(2) Reactor temperature\n \
-\t(3) Power level\n\n\
-Selection: ");
-    scanf("%d", &selection);
-
-    switch(selection){
-    case 1:
-        message = "WATER TEMPERATURE";
-        break;
-    case 2:
-        message = "REACTOR TEMPERATURE";
-        break;
-    case 3:
-        message = "POWER LEVEL";
-        break;
-    default:
-    	message = "*** Invalid selection.";
-        break;
+        puts("server recv failed");
     }
 
-    return message;
-}
+    if(str_compare(auth_passwd, buffer) == 0){
+    	// build a server_server socket
+    	while( server_socket == -1)
+    	{
+    		port_num = port_num < 65535 ? port_num + 1 : 49153;
+    		server_socket = server_socket_setup(port_num);
+    	}
 
-char* get_time_str(char server_reply[])
-{
-    time_t raw_time , current_time2;
-    char* c_time_string, c_time_string2;
-    char time_str[200];
-
-    /* Obtain current time as seconds elapsed since the Epoch. */
-    memcpy( time_str, &server_reply[0], 10);
-    time_str[10] = '\0';
-    //time_str = "1346426869";
-    raw_time  = atol(time_str);
-
-    if (raw_time  == ((time_t)-1))
-    {
-        (void) fprintf(stderr, "Failure to compute the current time.");
-        return 0;
-    }
-
-    /* Convert to local time format. */
-    c_time_string = ctime(&raw_time );
-    //puts(c_time_string);
-
-    if (c_time_string == NULL)
-    {
-        (void) fprintf(stderr, "Failure to convert the current time.");
-        return 0;
-    }
-
-    /* Print to stdout. */
-    c_time_string[strlen(c_time_string) - 1] = '\0';
-    return c_time_string;
-}
-
-char* get_value_string(char server_reply[])
-{
-    char value_string[100];
-    memcpy( value_string, &server_reply[11], strlen(server_reply) - 11);
-    value_string[strlen(server_reply) - 12] = '\0';
-    return value_string;
-}
-
-char* comm_with_server(int sock, struct sockaddr_in server, int phase)
-{
-    char server_reply[2000], return_msg[2000], msg[2000];
-    char* message;
-    static int selection;
-    memset(server_reply, 0, strlen(server_reply));
-    memset(return_msg, 0, strlen(return_msg));
-    memset(msg, 0, strlen(msg));
-
-    while(1)
-    {
-        if( phase == GET_AUTH ){
-            message = "AUTH secretpassword";
-        }else if ( phase == GET_CONN ){
-            message = "AUTH networks";
-        }else if ( phase == OPERATION ){
-            message = msg_for_operation(selection);
-        }else if ( phase == CLOSE ){
-            message = "CLOSE";
-        }
-        //printf("Enter message : ");
-        //scanf(" %[^\n]s", message);
-
-        //Send some data
-        strcat(msg, message);
-        strcat(msg, "\n");
-        if( send(sock , msg , strlen(msg) , 0) < 0)
+    	// tell client the new socket
+    	sprintf(msg, "CONNECT threemileisland.cs.dartmouth.edu %d networks\n"
+    			, port_num);;
+        if( send(conn_socket , msg , strlen(msg) , 0) < 0)
         {
-            puts("Send failed");
-            return 1;
+            puts("Telling client fail");
+            return -1;
         }
-
-        //Receive a reply from the server
-        //puts(server_reply);
-        if( recv(sock , server_reply , 2000 , 0) < 0)
-        {
-            puts("recv failed");
-            break;
-        }
-
-        //puts("Server reply :");
-        //puts(server_reply);
-        if( phase == GET_AUTH ){
-            memcpy( return_msg, &server_reply[41], 5 );
-            return_msg[5] = '\0';
-        }else if ( phase == OPERATION) {
-        	strcat(return_msg, "\n\t");
-            strcat(return_msg, "The last ");
-            strcat(return_msg, message);
-            strcat(return_msg, " was taken ");
-            strcat(return_msg, get_time_str(server_reply));
-            strcat(return_msg, " and was ");
-            strcat(return_msg, get_value_string(server_reply));
-            strcat(return_msg, "\n\n");
-        }else{
-            memcpy( return_msg, &server_reply[11], strlen(server_reply) );
-            return_msg[strlen(server_reply) ] = '\0';
-        }
-        return return_msg;
+        puts(msg);
     }
+	close(conn_socket);
+	return server_socket;
 }
 
-int talk_to_3mile(int port)
+int handle_clients(int conn_socket)
 {
-    int sock;
-    char* port_str;
-    struct sockaddr_in server;
+	char buffer[BUFFER_SIZE], msg[BUFFER_SIZE];
 
-    // Create socket
-    sock = create_socket();
+    // Tell client the operation port number
+	int server_socket = tell_port_num(conn_socket);
 
-    // Configure server info
-    if(port == -1)
-        server = config_server(47789);
-    else
-        server = config_server(port);
+	// handling new coming request
+	bzero(&threads, sizeof(pthread_t) * MAX_THREAD_NUM);
+	while (1)
+	{
+		struct sockaddr_in client_addr;
+		socklen_t length = sizeof(client_addr);
 
-    // Connect to remote server
-    if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
-    {
-        perror("connect failed. Error");
-        return 1;
-    }
+		// connect with client
+		int client_conn = accept(server_socket, (struct sockaddr*) &client_addr, &length);
+		if (client_conn < 0)
+		{
+			printf("Server Accept Failed in child!\n");
+			return EXIT_FAILURE;
+		}else{
+			puts("client found in child!");
+		}
 
-    if( port == -1 ){
-        // Get auth port
-        port_str = comm_with_server(sock, server, GET_AUTH);
-        if(!welcomed)
-        {
-            puts("WELCOME TO THE THREE MILE ISLAND SENSOR NETWORK\n\n");
-            welcomed = 1;
-        }
-    } else {
-        // hand shake
-        port_str = comm_with_server(sock, server, GET_CONN);
-        // operation
-        port_str = comm_with_server(sock, server, OPERATION);
-        puts(port_str);
-        // say bye
-        port_str = comm_with_server(sock, server, CLOSE);
-    }
-    close(sock);
-    return atoi(port_str);
+	    if( recv(client_conn , buffer , BUFFER_SIZE , 0) < 0)
+	    {
+	        puts("server recv failed");
+	    }
+
+	    if(str_compare(auth_network, buffer) == 0){
+	    	// tell client the new socket
+	    	sprintf(msg, "SUCCESS\n");;
+	        if( send(client_conn , msg , strlen(msg) , 0) < 0)
+	        {
+	            puts("Telling client fail");
+	            return -1;
+	        }
+	        puts(msg);
+	    }
+
+	}
+	close(server_socket);
+	return (EXIT_SUCCESS);
+
+	return 0;
 }
-
-int main()
-{
-    int port;
-
-    while(1){
-        port = talk_to_3mile(-1);
-        talk_to_3mile(port);
-    }
-
-    return 0;
-}
-
-
