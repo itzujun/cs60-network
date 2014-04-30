35be6ade7d1db20cbf676c7b587df8ac7e51766c
Junjie Guan <gjj2684@gmail.com>
2014-04-30 00:46:14 -0400

    add the bash scripts

---
+++ hw5/run.sh
@@ -0,0 +1,6 @@
+ssh gjj@tahoe.cs.dartmouth.edu   'bash -s' < local_run.sh
+ssh gjj@hooker.cs.dartmouth.edu  'bash -s' < local_run.sh
+ssh gjj@green.cs.dartmouth.edu   'bash -s' < local_run.sh
+ssh gjj@gile.cs.dartmouth.edu    'bash -s' < local_run.sh
+ssh gjj@bear.cs.dartmouth.edu    'bash -s' < local_run.sh
+ssh gjj@spruce.cs.dartmouth.edu  'bash -s' < local_run.sh
