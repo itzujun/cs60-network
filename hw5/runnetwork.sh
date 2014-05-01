2d483b0f9aa83cb32bd0a98f5430823db518260c
Junjie Guan <gjj@whaleback.cs.dartmouth.edu>
2014-04-30 20:41:37 -0400

    update scripts

--- hw5/runnetwork.sh
+++ hw5/runnetwork.sh
@@ -1,4 +1,4 @@
-ssh gjj@tahoe.cs.dartmouth.edu   'bash -s' < local_network.sh
-ssh gjj@hooker.cs.dartmouth.edu  'bash -s' < local_network.sh
-ssh gjj@gile.cs.dartmouth.edu    'bash -s' < local_network.sh
-ssh gjj@whaleback.cs.dartmouth.edu    'bash -s' < local_network.sh
+ssh gjj@tahoe.cs.dartmouth.edu         'bash' < local_network.sh
+ssh gjj@hooker.cs.dartmouth.edu        'bash' < local_network.sh
+ssh gjj@gile.cs.dartmouth.edu          'bash' < local_network.sh
+ssh gjj@whaleback.cs.dartmouth.edu     'bash' < local_network.sh
--- hw5/runoverlay.sh
+++ hw5/runoverlay.sh
@@ -1,4 +1,4 @@
-ssh gjj@tahoe.cs.dartmouth.edu       'bash -s' < local_overlay.sh
-ssh gjj@hooker.cs.dartmouth.edu      'bash -s' < local_overlay.sh
-ssh gjj@gile.cs.dartmouth.edu        'bash -s' < local_overlay.sh
-ssh gjj@whaleback.cs.dartmouth.edu   'bash -s' < local_overlay.sh
+ssh gjj@tahoe.cs.dartmouth.edu       'bash' < local_overlay.sh
+ssh gjj@hooker.cs.dartmouth.edu      'bash' < local_overlay.sh
+ssh gjj@gile.cs.dartmouth.edu        'bash' < local_overlay.sh
+ssh gjj@whaleback.cs.dartmouth.edu   'bash' < local_overlay.sh
