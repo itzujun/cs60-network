688ea4cdcae961a1fe451ee66b99d6b6f100a19a
Junjie Guan <gjj2684@gmail.com>
2014-04-30 20:38:49 -0400

    new scripts

--- hw5/run.sh
+++
@@ -1,6 +0,0 @@
-ssh gjj@tahoe.cs.dartmouth.edu   'bash -s' < local_run.sh
-ssh gjj@hooker.cs.dartmouth.edu  'bash -s' < local_run.sh
-ssh gjj@green.cs.dartmouth.edu   'bash -s' < local_run.sh
-ssh gjj@gile.cs.dartmouth.edu    'bash -s' < local_run.sh
-ssh gjj@bear.cs.dartmouth.edu    'bash -s' < local_run.sh
-ssh gjj@spruce.cs.dartmouth.edu  'bash -s' < local_run.sh
---
+++ hw5/runoverlay.sh
@@ -0,0 +1,4 @@
+ssh gjj@tahoe.cs.dartmouth.edu       'bash -s' < local_overlay.sh
+ssh gjj@hooker.cs.dartmouth.edu      'bash -s' < local_overlay.sh
+ssh gjj@gile.cs.dartmouth.edu        'bash -s' < local_overlay.sh
+ssh gjj@whaleback.cs.dartmouth.edu   'bash -s' < local_overlay.sh
