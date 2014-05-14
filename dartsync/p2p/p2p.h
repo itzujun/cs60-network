454aee296d2191b349807f3b4b04bf536f3d51aa
Junjie Guan <gjj2684@gmail.com>
2014-05-14 00:27:21 -0400

    Just finish drafting

--- dartsync/p2p/p2p.h
+++ dartsync/p2p/p2p.h
@@ -2,14 +2,26 @@
 #define DARTSYNCP2P
 #include "config.h"
 
+// P2PInfo type
 #define REQ 0
 #define REQFIN 1
 #define REREQ 2
 
+// piece status
+#define UNTOUCHED 0
+#define DONE 1
+#define DOWNLOADING 2
+#define RETRY 3
+
+// peer status
+#define NOTUSING 0
+#define USING 1
+
+// control msg that handle p2p connection
 typedef struct p2p_msg{
   int type;
   //the name of the file
-  char *name;
+  char name[FILE_NAME_LEN];
   // file szie
   int size;
   // the pieceNum in the file, start from 0
@@ -18,9 +30,10 @@ typedef struct p2p_msg{
   int srcPort;
   int destPort;
   // ip address
-  char* srcIp;
-  char* destIp;
-}P2PMsg;
+  char srcIp[NI_MAXHOST];
+  char destIp[NI_MAXHOST];
+
+}P2PInfo;
 
 /**
 pieces maintain table, 
@@ -34,10 +47,14 @@ typedef struct p2p_pieceTable{
   int piecesDone;
   // store total pieces
   int totalPieces;
+  // request info
+  P2PInfo* req;
   // keep track of which one is done
   int* pieces;
-  // keep track of which one is done
+  // keep track of which peer is using
   int* peers;
+  // thread handlers
+  pthread_t* threads;
   // the data area
   char* data;
   // pointer
@@ -52,20 +69,33 @@ void* p2p_upload(void* arg);
 
 int p2p_assemble(pEntry* myPieces);
 
-int p2p_download_start(char* filename);
+void* p2p_download_start(void* arg);
 
 /**********helper functions************/
+// socket communication related functions
 int listenSock_setup(int port);
 int connectToRemote(char* ip, int port);
 int check_P2PMsg(char* buff);
-int getPieceFromFile(char* piece, P2PMsg req);
-int initMyPieces(pEntry myPieces);
+char* getMyIp();
+int getAvailablePort();
+
+// pTable related functions
+int getPieceFromFile(char* piece, P2PInfo req);
+int getPieceToDownload(int &pNum, pEntry* myPieces);
+int initMyPieces(pEntry myPieces, P2PInfo* req);
+int addMyPieces2pTable(pEntry myPieces);
 pEntry* getMyPieces(char* name);
-int setMyPieces(char* piece, int pNum);
-int rmMyPieces(char* name);
+int writePieceData(pEntry* myPieces, char* piece, int pNum);
+int rmMyPieces(pEntry* myPieces);
+
+// thread related functions
+int startDownThread(pEntry* myPieces, int &threadCnt, void* arg);
+int startUpThread(pthread_t* tTable, int &threadCnt, void* arg);
+
+// P2PInfo related
+int initNewReq(P2PInfo* newReq, P2PInfo* req, int pNum, char* peerIP);
 
 /**********some temporate stuff for testing************/
-
 //each file can be represented as a node in file table
 typedef struct node{
 //the size of the file
@@ -81,5 +111,7 @@ typedef struct node{
 //newest edition of the file
   char *newpeerip;
 }Node,*pNode;
+
+int getPeerIPFromFT(char* peerIP, char* filename);
 
 #endif
